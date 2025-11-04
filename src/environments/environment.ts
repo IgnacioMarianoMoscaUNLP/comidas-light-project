dec4cd7bcd13be4b17e1ac153e3e822672cb034c
Ignacio Mariano <ignaciomarianomosca@gmail.com>
2025-11-04 11:28:00 -0300

    facebook link correction

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
   whatsappNumber: '5492215963237',
   whatsappMessage: 'Hola! Me interesa conocer más sobre las viandas saludables',
   instagramUrl: 'https://instagram.com/comidaslightviandas',
-  facebookUrl: 'https://facebook.com/comidaslight', // Ajusta la URL real
+  facebookUrl: 'https://facebook.com/viandasaludableslaplata', // Ajusta la URL real
   businessName: 'Comidas Light',
   businessLocation: 'Tolosa, La Plata'
 };
